43f4933cc488ff7a3d48046543e460f3423402c3
Tom van Dijk <t.vandijk@gmail.com>
2019-03-31 20:02:26 +0200

    Updated apt comments

--- src/apt.cpp
+++ src/apt.cpp
@@ -16,6 +16,7 @@
 
 #include <cassert>
 #include <cstring>
+#include <unistd.h>
 
 #include "apt.hpp"
 
@@ -35,22 +36,20 @@ APTSolver::run()
     unsigned int iterations = 0;
 
     // allocate and initialize data structures
-    int *F_in_Y = new int[n_nodes];
-    memset(F_in_Y, 0, sizeof(int[n_nodes]));
+    int8_t *distraction = new int8_t[n_nodes];
+    memset(distraction, 0, sizeof(int8_t[n_nodes]));
 
     for (;;) {
         iterations++;
 
         /**
-         * We update F_in_Y from low to high.
+         * We update distractions from low to high.
          * We process per block of priorities.
-         * When a block changes, all lower F_in_Y are reset.
+         * When a block changes, all lower priority vertices are reset.
          *
          * The algorithm refines a partition into two winning regions.
          * APT terminates when the winning regions are inductive, i.e.,
          * no player escapes in one step.
-         * If Y (set of vertices of priority pr not won by player pr&1) for some
-         * priority is updated, then all lower priorities are reset.
          */
         bool changed = false;
         int cur_pr = priority[0]; // priority of current block
@@ -58,8 +57,7 @@ APTSolver::run()
 
         for (int i=0; i<n_nodes;i++) {
             if (disabled[i]) continue;
-            // like Verver's Zielonka trick, not per priority change,
-            // but per parity change (mimics compression)
+            // on-the-fly compression like Verver's Zielonka trick
             if ((priority[i]&1) != (cur_pr&1)) {
                 // new block!
                 if (changed) break;
@@ -67,50 +65,64 @@ APTSolver::run()
                 cur_pr = priority[i];
                 first_idx = i;
             }
-            if (F_in_Y[i]) continue; // Y increases monotonically...
-            // compute Y for current vertex
+            if (distraction[i]) continue; // already a distraction (increases monotonically)
+            // now compute if the vertex is attracted to the opponent
             int y;
             if (owner[i] == 0) {
+                // see if player Even can go to a vertex currently good for Even
                 y = 1;
                 const int *_out = outs + outa[i];
                 for (int to = *_out; to != -1; to = *++_out) {
                     if (disabled[to]) continue;
-                    if ((priority[to] & 1) != F_in_Y[to]) continue;
+                    // check if good for Odd (odd priority XOR distraction)
+                    // a) pr[to] is Even and it's a distraction
+                    // b) pr[to] is Odd and it's not a distraction
+                    if ((priority[to] & 1) != distraction[to]) continue;
+                    // good for player Even
                     y = 0;
                     break;
                 }
             } else {
+                // see if player Odd can go to a vertex currently good for Odd
                 y = 0;
                 const int *_out = outs + outa[i];
                 for (int to = *_out; to != -1; to = *++_out) {
                     if (disabled[to]) continue;
-                    if ((priority[to] & 1) == F_in_Y[to]) continue;
+                    // check if good for Even (odd priority EQUIV distraction)
+                    // a) pr[to] is Even and it's not distraction
+                    // b) pr[to] is Odd and it's a distraction
+                    if ((priority[to] & 1) == distraction[to]) continue;
+                    // good for player Odd
                     y = 1;
                     break;
                 }
             }
-            // check if now in Y
-            if ((cur_pr&1) == y) continue; // not in Y
+            // if good-for-player equals vertex-parity, then it's not a distraction
+            if ((cur_pr&1) == y) continue;
             changed = true;
-            F_in_Y[i] = 1;
-            if (trace >= 2) logger << "vertex " << i << " is now in Y^" << cur_pr << std::endl;
+            distraction[i] = true;
+#ifndef NDEBUG
+            if (trace >= 2) logger << "vertex " << label_vertex(i) << " is now a distraction" << std::endl;
+#endif
         }
 
         if (!changed) break; // nothing changed, we're done
 
-        // the previous block changed, reset all F_in_Y from 0 to first_idx
-        memset(F_in_Y, 0, sizeof(int[first_idx]));
-        if (trace) logger << "restarting after update to Y of prio " << cur_pr << std::endl;
+        // the previous block changed, reset all distraction from 0 to first_idx
+        memset(distraction, 0, sizeof(int8_t[first_idx]));
+#ifndef NDEBUG
+        if (trace) logger << "restarting after finding distractions of prio " << cur_pr << std::endl;
+#endif
     }
 
     // done -- unfortunately, APT does not compute a strategy!
     for (int i=0; i<n_nodes; i++) {
         if (disabled[i]) continue;
-        oink->solve(i, F_in_Y[i] ? 1-(priority[i]&1) : (priority[i]&1), -1); 
+        oink->solve(i, distraction[i] ? 1-(priority[i]&1) : (priority[i]&1), -1);
     }
 
     // free allocated data structures
-    delete[] F_in_Y;
+    delete[] distraction;
 
     logger << "solved with " << iterations << " iterations." << std::endl;
 }
