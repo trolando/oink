74cc23fcd0435807fd43aa03abda38e7bc11ecae
pkubaj <pkubaj@FreeBSD.org>
2022-01-14 18:12:45 +0000

    Fix build on powerpc*
    mfence equivalent on powerpc* is sync.

--- src/lace.h
+++ src/lace.h
@@ -293,7 +293,11 @@ void lace_yield(WorkerP *__lace_worker, Task *__lace_dq_head);
 #endif
 
 #ifndef mfence
+#if defined(__amd64__) || defined(__i386__)
 #define mfence() { asm volatile("mfence" ::: "memory"); }
+#elif defined(__powerpc__)
+#define mfence() { asm volatile("sync" ::: "memory"); }
+#endif
 #endif
 
 /* Compiler specific branch prediction optimization */
